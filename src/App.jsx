de14ae90608d19f5f8695bbbedb3afe3e3ab6791
Victory Diemuodeke <Victorydiemuodeke@gmail.com>
2026-09-11 12:27:43 +0100

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import { useState } from 'react'
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
- 
 import Home from './Pages/Home'
-// import Job from './Pages/Job'
-// import Company from './Pages/Company'
 import About from './Pages/About'
-// import Pricing from './Pages/Pricing'
-// import BlogPost from './Component/BlogPage/BlogPost'
 import Contact from './Pages/Contact'
-// import Blog from './Pages/Blog'
 import './App.css'
-
 import Navbar from './Pages/Navbar'
 import Footer from './Pages/Footer'
 import ScrollToTop from './Components/ScrollToTopEffect'
 import ScrollProgress from './Components/ScrollProgress'
-import Program from './Components/HomepageComponents/Programs'
 import ProgramPage from './Pages/Program'
 
 function App() {
